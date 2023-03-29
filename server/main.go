87f8a754412dfcd7f4ec425056b07401a8f9e455
neysanatasha <rikilah930@gmail.com>
2023-03-29 11:48:36 +0700

    redeploy

---
+++ server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"holyways/database"
+	"holyways/pkg/mysql"
+	"holyways/routes"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("failed to load env file")
+	}
+
+	e := echo.New()
+	mysql.DatabaseInit()
+	database.RunMigration()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE, echo.PUT},
+		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
+	}))
+
+	PORT := os.Getenv("PORT")
+
+	routes.RouteInit(e.Group("/api/v1"))
+	e.Static("/uploads", "./uploads")
+
+	fmt.Println("Server running on localhost:" + PORT)
+	e.Logger.Fatal(e.Start(":" + PORT))
+}
